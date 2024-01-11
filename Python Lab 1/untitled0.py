ef40d3d89bfdad1c94cbac45e5ceb501ee52e72b
Aaryan Tiwari <153616544+BlueMoonerATcode@users.noreply.github.com>
2024-01-11 21:55:20 +0530

    Update untitled0.py

--- Python Lab 1/untitled0.py
+++ Python Lab 1/untitled0.py
@@ -8,4 +8,4 @@ Created on Wed Jan 10 10:15:28 2024
 num1 = 10  
 num2 = 20
 sum = num1 + num2
-print("The sum of", num1, "and", num2, "is:", sum)
+print("The sum of ", num1, "and", num2, "is:", sum)
